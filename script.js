256ff752368e4149df787c35e41de86eda6d085c
yungsasha777 <yungsasha777@gmail.com>
2021-11-14 13:49:08 +0300

    script

---
+++ script.js
@@ -0,0 +1,4 @@
+const url = 'https://api.telegram.org/bot2113123167:AAGb4aN4FICy_YBzgE2fHsO_kfQeIKwBovA/sendMessage?chat_id=953909659&text=someone+visited+your+website'
+const xhttp = new XMLHttpRequest();
+xhttp.open('GET', url)
+xhttp.send();
